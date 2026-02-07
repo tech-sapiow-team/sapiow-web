2a9991c589cdc1ef04730508d55a81c9b4983a18
takonguina <tommy.akonguina@yahoo.fr>
2026-02-07 17:58:25 +0100

    fix(image): load images from prod url

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "ncgpnkqqjijnecpbflwm.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async headers() {
